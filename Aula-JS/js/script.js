c7b368a6d91ed1cdda8b204d7dab22be77c25b2b
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-05-21 17:22:18 -0300

    Add files via upload

---
+++ Aula-JS/js/script.js
@@ -0,0 +1,50 @@
+//variáveis definidas
+
+let nome="Eduardo";
+let idade=17;
+let hobbie="Ping-pong";
+
+console.log(nome);
+console.log(idade);
+console.log(hobbie);
+
+//PI
+
+const PI=3.14159;
+let raio=5;
+let area=PI*raio*raio;
+console.log(area)
+
+//variáveis 2
+
+let nome2="João";
+let idade2=30;
+let ativo=true;
+
+console.log(typeof nome2);
+console.log(typeof idade2);
+console.log(typeof ativo);
+
+//multiplicação
+
+let stringNumero="50";
+let numero=Number (stringNumero);
+let resultado=numero + 10;
+console.log (resultado);
+
+//mensagem
+
+let mensagem="Fora"; {
+    let mensagem="Dentro";
+    console.log(mensagem);
+}
+
+console.log(mensagem);
+
+//variáveis 3
+
+let nome3="Juscelino";
+let idade3=47;
+let cidade="BNU";
+
+console.log(nome3, idade3, cidade);
