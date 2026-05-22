0383c6233f24cdf6ef51dbbd8455935c3c1a545d
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-05-22 15:43:56 -0300

    add-aula-02-js

---
+++ Aula-02-Js/Js/script.js
@@ -0,0 +1,12 @@
+let nome="Baymax";
+console.log(nome);
+
+const nivel=10;
+console.log(nivel);
+
+let ativo="true";
+console.log(ativo);
+
+console.log(typeof nome);
+console.log(typeof nivel);
+console.log(typeof ativo);
