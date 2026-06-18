d2675864b2c4eafc7c7004742ab6689781aa8da8
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-06-18 13:41:25 -0300

    Add files via upload

---
+++ Aula-05-Js/Feito com a professora/js/script.js
@@ -0,0 +1,20 @@
+/* let soma = 0  
+for (let  i = 0; i < 5; i++) {
+    
+    soma = soma + 2;
+    alert (soma)
+} */
+ 
+/* for (let  i = 0; i <= 15; i++) {
+    if (i % 2 == 0) {
+        console.log ("Número par: " + i)
+    } else {
+        console.log ("Número ímpar: " + i)
+    }
+} */
+
+/* let opcao = "não"
+while (opcao != "sim") {
+    alert ("Olá, seja bem-vindo! | " + opcao)
+    opcao = prompt ("Quer parar? 'sim' para - 'não' - continua")
+} */
